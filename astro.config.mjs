4f974cb686709483cb624bf5550065bc3ef70586
Pablo Vega <materializacreative@gmail.com>
2026-07-21 23:32:03 +0200

    robots, site maps y correcciones

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,10 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
+import sitemap from '@astrojs/sitemap';
+import robotsTxt from 'astro-robots-txt';
 
 // https://astro.build/config
 export default defineConfig({
-    site: 'https://daarkling.vercel.app',
+  site: 'https://daarkling.vercel.app',
+  integrations: [sitemap(), robotsTxt()],
 });
